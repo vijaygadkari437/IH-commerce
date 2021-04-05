4162a3074709c538073eeb6d4cc5c0b9776ebef2
Lubna Khan <klubna17@gmail.com>
2021-04-05 19:00:56 +0530

    DataTable Added in Customer

--- app/components/FormHeading/index.js
+++ app/components/FormHeading/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(theme => ({}));
+const useStyles = makeStyles(theme => ({
+  wrapper: {
+    height: '200',
+    boxShadow: '0 0 0 1px rgba(63, 63, 68, 0.10)',
+    backgroundColor: '#fff',
+    padding: 30,
+    borderRadius: 4,
+    marginBottom: 20,
+  },
+}));
 export default function FormHeading(props) {
   const classes = useStyles();
-  return <h2>{props.label}</h2>;
+  return (
+    <Fragment>
+      <h2>{props.label}</h2>
+      <div className={classes.wrapper} />
+    </Fragment>
+  );
 }
