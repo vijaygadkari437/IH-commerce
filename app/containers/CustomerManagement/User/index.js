724ebc12342d266d22d36e3e716baad7e79d1ff9
Lubna Khan <klubna17@gmail.com>
2021-04-24 19:15:45 +0500

    Data Grid package updated

--- app/containers/CustomerManagement/User/index.js
+++ app/containers/CustomerManagement/User/index.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { DataGrid } from '@material-ui/data-grid';
 import Form from '../../../components/FormFields';
 import FormHeading from '../../../components/FormHeading';
 
@@ -122,13 +123,55 @@ export default function User() {
       ],
     },
   ];
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'firstName', headerName: 'First name', width: 130 },
+    { field: 'lastName', headerName: 'Last name', width: 130 },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      width: 90,
+    },
+    {
+      field: 'fullName',
+      headerName: 'Full name',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160,
+      valueGetter: params =>
+        `${params.getValue('firstName') || ''} ${params.getValue('lastName') ||
+          ''}`,
+    },
+  ];
+
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  ];
+
   return (
     <Fragment>
-      <FormHeading label="User" buttontext="Back"  grid>
-        {fields.map(fields => (
-          <Form {...fields} />
+      <FormHeading label="User" buttontext="Back" grid>
+        {fields.map(field => (
+          <Form {...field} />
         ))}
       </FormHeading>
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          checkboxSelection
+        />
+      </div>
     </Fragment>
   );
 }
